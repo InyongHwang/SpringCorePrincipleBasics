dcd28578856d9e517fee097db684dac8ce8d7210
InyongHwang <hiy1934@gmail.com>
2023-04-25 17:41:02 +0900

    1. 새로운 할인 정책 적용 적용 과정에서 관심사 분리 수행. AppConfig.java에 의해 의존성 주입받게 됨

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -7,7 +7,9 @@ import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = new MemberServiceImpl();
+        MemberService memberService = appConfig.memberService();
 
         // 회원 가입
         Member member = new Member(1L, "memberA", Grade.VIP);
